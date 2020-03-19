f4040a7cd0043bdf49d63a06db0e5a20f0ef57b6
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-03-18 20:44:01 -0700

    Got blog post lists working

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent {
+  constructor(private scully: ScullyRoutesService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  $blogPosts = this.scully.available$.pipe(
+    map(routes =>
+      routes.filter(
+        route =>
+          route.route.startsWith('/blog/') && route.sourceFile.endsWith('.md')
+      )
+    )
+  );
 }
